983bc036122e41bab9695b29aa25c040896ab981
Laïla Charaoui <54016834+lailacha@users.noreply.github.com>
2020-05-28 18:45:36 +0200

    Initialisation du projet

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index()
+    {
+        return $this->render('personnage/index.html.twig');
+    }
+
+    /**
+     * @Route("/persos", name="personnages")
+     */
+    public function persos()
+    {
+        return $this->render('personnage/persos.html.twig', [
+            "pseudo" => "toto",
+            "age" => 25,
+            "carac" => [
+                "force" => 3,
+                "agi" => 2,
+                "intel" => 3
+            ]
+        ]);
+    }
+}
