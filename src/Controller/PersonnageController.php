fbe4e43755db2fb7c1acd1cddf35b1d2f9356f58
Laïla Charaoui <54016834+lailacha@users.noreply.github.com>
2020-10-06 21:25:16 +0200

    v2

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -20,14 +20,48 @@ class PersonnageController extends AbstractController
      */
     public function persos()
     {
-        return $this->render('personnage/persos.html.twig', [
-            "pseudo" => "toto",
+        $j1 = [
+            "nom" => "Marc",
             "age" => 25,
+            "sexe" => true,
             "carac" => [
                 "force" => 3,
                 "agi" => 2,
                 "intel" => 3
+
             ]
+        ];
+        $j2 = [
+            "nom" => "Milo",
+            "age" => 30,
+            "sexe" => true,
+            "carac" => [
+                "force" => 3,
+                "agi" => 1,
+                "intel" => 3
+
+            ]
+        ];
+
+        $j3 = [
+            "nom" => "Tya",
+            "age" => 22,
+            "sexe" => false,
+            "carac" => [
+                "force" => 3,
+                "agi" => 2,
+                "intel" => 3
+
+            ]
+        ];
+        $players = [
+            "j1" => $j1,
+            "j2" => $j2,
+            "j3" => $j3
+        ];
+
+        return $this->render('personnage/persos.html.twig', [
+            "players" => $players
         ]);
     }
 }
